57c8ccdd21181d869a73a794281223274d4584be
“davidmagbee” <david.magbee@me.com>
2024-04-11 18:00:49 -0400

    finish setting up base nav && popover menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,11 @@
 "use client";
 
-import { Bars3Icon, PaperAirplaneIcon, PhoneIcon, PlayCircleIcon } from "@heroicons/react/20/solid";
+import {
+  Bars3Icon,
+  PaperAirplaneIcon,
+  PhoneIcon,
+  PlayCircleIcon,
+} from "@heroicons/react/20/solid";
 import Link from "next/link";
 import { useState, Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
@@ -9,15 +14,30 @@ import { HomeIcon } from "@heroicons/react/20/solid";
 import { ChatBubbleOvalLeftIcon } from "@heroicons/react/20/solid";
 
 const products = [
-    { name: "Book a Room", description: 'Book a room on Booking.com', href: "#", icon: HomeIcon },
-    { name: "Book a Trip", description: 'Book a Flight', href: "#", icon: PaperAirplaneIcon },
-    { name: "Contact our Support Team", description: 'Get assistance with your booking.', href: "#", icon: ChatBubbleOvalLeftIcon },
+  {
+    name: "Book a Room",
+    description: "Book a room on Booking.com",
+    href: "#",
+    icon: HomeIcon,
+  },
+  {
+    name: "Book a Trip",
+    description: "Book a Flight",
+    href: "#",
+    icon: PaperAirplaneIcon,
+  },
+  {
+    name: "Contact our Support Team",
+    description: "Get assistance with your booking.",
+    href: "#",
+    icon: ChatBubbleOvalLeftIcon,
+  },
 ];
 
 const callsToAction = [
-    { name: 'See Demo Booking', href: '#', icon: PlayCircleIcon },
-    { name: 'Contact Support', href: '#', icon: PhoneIcon },
-]
+  { name: "See Demo Booking", href: "#", icon: PlayCircleIcon },
+  { name: "Contact Support", href: "#", icon: PhoneIcon },
+];
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -69,25 +89,72 @@ function Header() {
             >
               <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
                 <div className="p-4">
-                    {products.map((item) => (
-                        <div key={item.name} className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50">
-                            <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-gray-200">
-                                <item.icon className="h-6 w-6 text-[#013B94] group-hover:text-blue-600" aria-hidden="true" />
-                            </div>
-                            <div className="flex-auto">
-                                <Link href={item.href} className="block font-semibold text-[#013B94]">
-                                    {item.name}
-                                    <span className="absolute inset-0" />
-                                </Link>
-                                <p className="mt-1 text-[#013B94]">{item.description}</p>
-                            </div>
-                        </div>
+                  {products.map((item) => (
+                    <div
+                      key={item.name}
+                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                    >
+                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-gray-200">
+                        <item.icon
+                          className="h-6 w-6 text-[#013B94] group-hover:text-blue-600"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <div className="flex-auto">
+                        <Link
+                          href={item.href}
+                          className="block font-semibold text-[#013B94]"
+                        >
+                          {item.name}
+                          <span className="absolute inset-0" />
+                        </Link>
+                        <p className="mt-1 text-[#013B94]">
+                          {item.description}
+                        </p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+
+                <div>
+                  <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
+                    {callsToAction.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-[#013B94] hover:bg-gray-100"
+                      >
+                        <item.icon
+                          className="h-5 w-5 flex-none"
+                          aria-hidden="true"
+                        />
+                        {item.name}
+                      </a>
                     ))}
+                  </div>
                 </div>
               </Popover.Panel>
             </Transition>
           </Popover>
+          {/* TODO: refactor as an array and map over */}
+          <a href="#" className="text-sm font-semibold leading-6 text-white">
+            Flights
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-white">
+            Car Rentals
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-white">
+            Attractions
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-white">
+            Flight + Hotel
+          </a>
         </Popover.Group>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <Link href="#" className="text-sm font-semibold leading-6 text-white">
+            Log in <span aria-hidden="true">&rarr;</span>
+          </Link>
+        </div>
       </nav>
     </header>
   );
