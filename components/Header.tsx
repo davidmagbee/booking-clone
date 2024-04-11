2ed2300d75bb98dff1b269085483287e16b87711
“davidmagbee” <david.magbee@me.com>
2024-04-11 17:42:23 -0400

    initial app setup, install: headlessui, heroicons, being: Header component

---
+++ components/Header.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { Bars3Icon, PaperAirplaneIcon, PhoneIcon, PlayCircleIcon } from "@heroicons/react/20/solid";
+import Link from "next/link";
+import { useState, Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "lucide-react";
+import { HomeIcon } from "@heroicons/react/20/solid";
+import { ChatBubbleOvalLeftIcon } from "@heroicons/react/20/solid";
+
+const products = [
+    { name: "Book a Room", description: 'Book a room on Booking.com', href: "#", icon: HomeIcon },
+    { name: "Book a Trip", description: 'Book a Flight', href: "#", icon: PaperAirplaneIcon },
+    { name: "Contact our Support Team", description: 'Get assistance with your booking.', href: "#", icon: ChatBubbleOvalLeftIcon },
+];
+
+const callsToAction = [
+    { name: 'See Demo Booking', href: '#', icon: PlayCircleIcon },
+    { name: 'Contact Support', href: '#', icon: PhoneIcon },
+]
+
+function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <header className="bg-[#013B94]">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1">
+          <Link href="/" className="-m-1.5 p-1.5">
+            <span className="sr-only">Booking.com Clone</span>
+            <img
+              className="h-12 w-auto"
+              src="https://cf.bstatic.com/static/img/bcom_logo_blue_bg/f12f834e849b2a7f752a14b2598a6ddfeda1e713.svg"
+              alt="Booking.com Logo"
+            />
+          </Link>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
+            onClick={() => setIsMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+
+        <Popover.Group className="hidden lg:flex lg:gap-x-12">
+          <Popover className="relative">
+            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-white">
+              Stays
+              <ChevronDownIcon
+                className="h-5 w-5 flex-none"
+                aria-hidden="true"
+              />
+            </Popover.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+                <div className="p-4">
+                    {products.map((item) => (
+                        <div key={item.name} className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50">
+                            <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-gray-200">
+                                <item.icon className="h-6 w-6 text-[#013B94] group-hover:text-blue-600" aria-hidden="true" />
+                            </div>
+                            <div className="flex-auto">
+                                <Link href={item.href} className="block font-semibold text-[#013B94]">
+                                    {item.name}
+                                    <span className="absolute inset-0" />
+                                </Link>
+                                <p className="mt-1 text-[#013B94]">{item.description}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </Popover>
+        </Popover.Group>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
