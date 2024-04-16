6abc2bf9bd506057863af053f8ae01e9f6e3ac74
“davidmagbee” <david.magbee@me.com>
2024-04-16 11:37:49 -0400

    add popover calendar and addl ui components

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -4,6 +4,9 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+
 
 import { Button } from "@/components/ui/button";
 
@@ -16,6 +19,9 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { BedDoubleIcon, CalendarIcon } from "lucide-react";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { Calendar } from "./ui/calendar";
 
 export const formSchema = z.object({
   location: z.string().min(2, "Must be at least 2 characters.").max(50),
@@ -61,7 +67,88 @@ function SearchForm() {
       <form
         onSubmit={form.handleSubmit(onSubmit)}
         className="flex flex-col lg:flex-row lg:max-w-6xl lg:mx-auto items-center justify-center space-x-0 lg:space-x-2 space-y-4 lg:space-y-0 rounded-lg"
-      ></form>
+      >
+        <div className="grid w-full lg:max-w-sm items-center gap-1.5">
+          <FormField
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-white flex">
+                  Location
+                  <BedDoubleIcon className="ml-2 h-4 w-4 text-white" />
+                </FormLabel>
+
+                <FormMessage />
+
+                <FormControl>
+                  <Input
+                    placeholder="Where are you going?"
+                    {...field}
+                    type="text"
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="grid w-full lg:max-w-sm flex-1 items-center gap-1.5">
+          <FormField
+            control={form.control}
+            name="dates"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel className="text-white">Dates</FormLabel>
+                <FormMessage />
+
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        id="date"
+                        name="dates"
+                        variant={"outline"}
+                        className={cn(
+                          "w-full lg:w-[300px] justify-start text-left font-normal",
+                          !field.value.from && "text-muted-foreground"
+                        )}
+                      >
+                        <CalendarIcon className="mr-3 h-4 w-4 opacity-50" />
+                        {field.value?.from ? (
+                          field.value?.to ? (
+                            <>
+                              {format(field.value?.from, "LLL dd, y")} -{" "}
+                              {format(field.value?.to, "LLL dd, y")}
+                            </>
+                          ) : (
+                            format(field.value?.from, "LLL dd, y")
+                          )
+                        ) : (
+                          <span>Select your dates</span>
+                        )}
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      initialFocus
+                      mode="range"
+                      selected={field.value}
+                      defaultMonth={field.value.from}
+                      onSelect={field.onChange}
+                      numberOfMonths={2}
+                      disabled={(date) =>
+                        date < new Date(new Date().setHours(0, 0, 0, 0))
+                      }
+                    />
+                  </PopoverContent>
+                </Popover>
+              </FormItem>
+            )}
+          />
+        </div>
+      </form>
     </Form>
   );
 }
