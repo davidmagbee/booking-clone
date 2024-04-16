f353127dcedc20cecc6ff9434b956743ae43b0e5
“davidmagbee” <david.magbee@me.com>
2024-04-16 11:42:09 -0400

    add adults, children, room selection

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -7,7 +7,6 @@ import * as z from "zod";
 import { cn } from "@/lib/utils";
 import { format } from "date-fns";
 
-
 import { Button } from "@/components/ui/button";
 
 import {
@@ -147,6 +146,56 @@ function SearchForm() {
               </FormItem>
             )}
           />
+        </div>
+
+        <div className="flex w-full items-center space-x-2">
+          <div className="grid items-center flex-1">
+            <FormField
+              control={form.control}
+              name="adults"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel className="text-white">Adults</FormLabel>
+                  <FormMessage />
+                  <FormControl>
+                    <Input type="number" placeholder="Adults" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div className="grid items-center flex-1">
+            <FormField
+              control={form.control}
+              name="children"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel className="text-white">Children</FormLabel>
+                  <FormMessage />
+                  <FormControl>
+                    <Input type="number" placeholder="Children" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div className="grid items-center flex-1">
+            <FormField
+              control={form.control}
+              name="rooms"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel className="text-white">Rooms</FormLabel>
+                  <FormMessage />
+                  <FormControl>
+                    <Input type="number" placeholder="rooms" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
         </div>
       </form>
     </Form>
