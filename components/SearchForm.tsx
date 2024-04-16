ff5887a078be4bfc9500d2a0173f632d08c6a475
“davidmagbee” <david.magbee@me.com>
2024-04-16 11:22:55 -0400

    initial SearchForm setup, zodResolver setup, install ui components

---
+++ components/SearchForm.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+export const formSchema = z.object({
+  location: z.string().min(2, "Must be at least 2 characters.").max(50),
+  dates: z.object({
+    from: z.date(),
+    to: z.date(),
+  }),
+  adults: z
+    .string()
+    .min(1, {
+      message: "Please select at least 1 adult",
+    })
+    .max(12, { message: "Max 12 adults Occupancy" }),
+  children: z.string().min(0).max(12, {
+    message: "Max 12 children Occupancy",
+  }),
+  rooms: z.string().min(1, {
+    message: "Please select at least 1 room",
+  }),
+});
+
+function SearchForm() {
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      location: "",
+      dates: {
+        from: undefined,
+        to: undefined,
+      },
+      adults: "1",
+      children: "0",
+      rooms: "1",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {}
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col lg:flex-row lg:max-w-6xl lg:mx-auto items-center justify-center space-x-0 lg:space-x-2 space-y-4 lg:space-y-0 rounded-lg"
+      ></form>
+    </Form>
+  );
+}
+
+export default SearchForm;
